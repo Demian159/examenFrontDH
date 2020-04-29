fe2aaf9d10cc04a16733807fbf5b52841c461899
unknown <adrian_salatino@hotmail.com>
2020-04-29 18:15:11 -0300

    Contenido

---
+++ Instrucciones y contenido/CONSIGNAS/js/script10.js
@@ -0,0 +1,42 @@
+window.addEventListener( // NO TOCAR
+  'load',  // NO TOCAR
+  function() // NO TOCAR
+  { // NO TOCAR
+
+
+    // 10.1)
+    // ESCRIBIR AQUI
+
+
+
+
+
+    // 10.2)
+    // ESCRIBIR AQUI
+
+
+
+
+    // 10.3)
+    // ESCRIBIR AQUI
+
+
+
+
+
+    // 10.4)
+    // ESCRIBIR AQUI
+
+
+
+
+
+
+
+
+
+
+
+
+  }// NO TOCAR
+);// NO TOCAR
